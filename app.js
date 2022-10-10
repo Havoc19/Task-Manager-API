1f288646af149ba1eaf2e822e655d5332803811f
Akarsh <akarshmaurya19@gmail.com>
2022-10-10 20:02:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const tasks = require('./routes/task')
+const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+require('dotenv').config()
+
+app.use(express.static("./public"))
+app.use(express.json());
+
+app.use('/api/v1/tasks',tasks);
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+    } catch(error) {
+        console.log(error)
+    }
+}
+ 
+start()
